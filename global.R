a5396552fa5b881f8feaa3de4839eb8c98563fc1
unknown <venkatanarayanan22@gmail.com>
2020-11-28 01:08:47 +0530

    Added code for calculating category variable

--- global.R
+++ global.R
@@ -7,6 +7,8 @@ library(magrittr)
 library(scales)
 library(ggradar)
 library(DT)
+library(ggtext)
+library(gghighlight)
 
 big_5_combined <- read.csv("big_5_data.csv")
 
@@ -21,6 +23,13 @@ big_5_combined$Pos <- str_replace(big_5_combined$Pos, "MF,FW", "MF")
 big_5_combined$Pos <- str_replace(big_5_combined$Pos, "MF,DF", "MF")
 big_5_combined$Pos <- str_replace(big_5_combined$Pos, "FW,DF", "FW")
 big_5_combined$Pos <- str_replace(big_5_combined$Pos, "FW,MF", "FW")
+
+
+big_5_combined %<>%
+  group_by(Squad) %>%
+  mutate(category = ifelse(Gls == max(Gls), "Most Goals",
+                           ifelse(Min == max(Min), "Most Minutes",
+                                  ifelse(Ast == max(Ast), "Most Assists", "Normal"))))
 
 leagueNames <- as.character(unique(big_5_combined$league))
 
