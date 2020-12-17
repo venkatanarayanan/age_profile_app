58d714bcea1b50b2ce298a0dd80bcc95dc7da8f9
unknown <venkatanarayanan22@gmail.com>
2020-12-17 14:43:45 +0530

    added code to calculate age group with max. number of players

--- global.R
+++ global.R
@@ -11,6 +11,7 @@ library(DT)
 library(ggtext)
 library(gghighlight)
 library(ggiraph)
+library(ggnewscale)
 
 big_5_combined <- read.csv("big_5_data.csv")
 
@@ -48,5 +49,33 @@ leagueNames <- as.character(unique(big_5_combined$league))
 
 
 
+max_age_group_team <- big_5_combined %>%
+    count(Squad, age_group) %>%
+    slice(which.max(n)) %>%
+    rename("max_age_group" = "age_group") %>%
+    mutate(min_years = ifelse(max_age_group == "Under-23", NA,
+                            ifelse(max_age_group == "23-25 years", 23 , 
+                                   ifelse(max_age_group == "26-29 years", 26,
+                                          30))),
+           max_years = ifelse(max_age_group == "Under-23", 23,
+                            ifelse(max_age_group == "23-25 years", 25 , 
+                                   ifelse(max_age_group == "26-29 years", 29,
+                                          NA))))
 
 
+min_max_age_team <- big_5_combined %>%
+    filter(Min > 0) %>%
+    group_by(Squad) %>%
+    summarise(min_age = min(Age), max_age = max(Age))
+
+
+age_area_data <- max_age_group_team %>%
+    left_join(min_max_age_team) %>%
+    mutate(min_years = ifelse(is.na(min_years), min_age, min_years),
+           max_years = ifelse(is.na(max_years), max_age, max_years))
+
+
+
+
+big_5_combined %<>%
+  left_join(age_area_data, by = "Squad")
